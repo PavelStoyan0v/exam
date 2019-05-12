0c193f0c71907c5a1e4485be3b92b13434e498c0
PavelStoyan0v <32341455+PavelStoyan0v@users.noreply.github.com>
2019-05-12 12:37:01 +0300

    Add files via upload

---
+++ Class/Order.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+public class Order
+{
+    public int orderNumber;
+    public List<Product> productsList;
+    public static int OrdersCount = 0;
+
+    public Order(int orderNumber, List<Product> productsList)
+    {
+        this.orderNumber = orderNumber;
+        this.productsList = productsList;
+        Interlocked.Increment(ref OrdersCount);
+    }
+
+    ~Order()
+    {
+        Interlocked.Decrement(ref OrdersCount);
+    }
+
+    public void AddProduct(Product product)
+    {
+        this.productsList.Add(product);
+    }
+
+
+
+    public double GetOrderTotalPrice()
+    {
+        double price = 0;
+        foreach (var product in productsList)
+        {
+            price += product.Price;
+        }
+
+        return price;
+    }
+
+
+
+    public double GetDiscountedProductsTotalPrice()
+
+    {
+        double price = 0;
+        foreach(var product in productsList)
+        {
+            if(product.isOnPromotion)
+                price += product.Price;
+        }
+
+        return price;
+    }
+
+
+    //double ?
+    public double GetDiscountedProductsCount()
+    {
+        int count = 0;
+        foreach(var product in productsList)
+        {
+            if (product.isOnPromotion)
+                count++;
+        }
+
+        return count;
+    }
+
+
+
+    public override string ToString()
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.AppendLine($"Order #{this.orderNumber} has the following products:");
+        foreach (var product in this.productsList)
+        {
+            sb.AppendLine($"### {product}");
+        }
+
+        return sb.ToString();
+    }
+}
---
+++ Class/Product.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public class Product
+{
+    private string _name;
+    public string Name
+    {
+        get
+        {
+            return _name;
+        }
+        set
+        {
+            if (value.Length < 3)
+                throw new ArgumentException("Invalid product name!");
+
+            if (!Regex.IsMatch(value, "[a-zA-Z0-9]+"))
+                throw new Exception("Invalid product name!");
+
+            this._name = value;
+        }
+    }
+
+    private double _price;
+
+    public double Price
+    {
+        get
+        {
+            if (this.isOnPromotion)
+                return _price * 0.8;
+            return _price;
+        }
+        set
+        {
+            if (value < 0)
+                throw new ArgumentException("Price should be positive!");
+
+            _price = value;
+        }
+    }
+    public bool isOnPromotion;
+
+    public Product(string name, double price)
+    {
+        this.Name = name;
+        this.Price = price;
+    }
+
+    public Product(string name, double price, bool isOnPromotion)
+    {
+        this.Name = name;
+        this.Price = price;
+        this.isOnPromotion = isOnPromotion;
+    }
+
+    public override string ToString()
+    {
+        string promotion = this.isOnPromotion ? "YES" : "NO";
+        return $"Product -> {this.Name} with price {this.Price:f2}. On promotion: {promotion}";
+    }
+}
